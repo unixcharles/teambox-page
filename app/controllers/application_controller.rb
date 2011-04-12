36c231ccbc8c1df5d09ac51693e2260a8f021975
Charles Barbier <unixcharles@gmail.com>
2011-04-12 15:29:26 -0700

    Fix redirection loop when database is empty

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :find_setting, :redirect_to_login, :find_current_user
+  before_filter :find_setting, :find_current_user
 
   private
   def find_current_user
@@ -11,12 +11,6 @@ class ApplicationController < ActionController::Base
 
   def find_setting
     @setting = Setting.find_or_create_by(domain: request.host)
-  end
-
-  def redirect_to_login
-    if @setting.nil? or (@setting.routes.empty? or @setting.pages.empty?)
-      redirect_to login_url
-    end    
   end
 
   def find_pages
